6e872690e80b0d930db2fb4b35d729ccf704a153
WhoHarsh-14 <ps4042927@gmail.com>
2022-04-21 18:11:25 +0530

    Fixed Minor Bugs

--- src/main/java/me/harsh/privategamesaddon/events/InventoryListener.java
+++ src/main/java/me/harsh/privategamesaddon/events/InventoryListener.java
@@ -1,15 +1,11 @@
 package me.harsh.privategamesaddon.events;
 
-import de.marcely.bedwars.api.GameAPI;
-import de.marcely.bedwars.api.arena.Arena;
-import de.marcely.bedwars.api.arena.ArenaStatus;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.inventory.ItemStack;
-import org.mineacademy.fo.Common;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.Inventory;
 
 
 public class InventoryListener implements Listener {
@@ -17,21 +13,10 @@ public class InventoryListener implements Listener {
 
     @EventHandler(priority = EventPriority.HIGH)
     public void onInventoryClick(InventoryClickEvent event){
-        final ItemStack item = event.getCurrentItem();
-        if (!(event.getWhoClicked() instanceof  Player)) return;
-        final Player player = ((Player) event.getWhoClicked()).getPlayer();
-        final Arena arena = GameAPI.get().getArenaByPlayer(player);
-        if (arena != null){
-            if (arena.getStatus() == ArenaStatus.LOBBY){
-                GameAPI.get().getLobbyItems().forEach(lobbyItem -> {
-                    if (lobbyItem.getItem() == item){
-                        Common.log("Blocking movement of lobby item");
-                        event.setCancelled(true);
-                    }else {
-                        event.setCancelled(false);
-                    }
-                });
-            }
+        final Inventory inventory = event.getClickedInventory();
+        if (inventory != null && inventory.getType() != InventoryType.PLAYER){
+            event.setCancelled(false);
         }
+
     }
 }
