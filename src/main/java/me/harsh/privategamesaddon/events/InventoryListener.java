3bf3a862e96e0681b660fb78deb0770e36f4662e
Harsh <82308380+WhoHarsh-14@users.noreply.github.com>
2022-05-01 20:14:07 +0530

    Fixed menu issue

--- src/main/java/me/harsh/privategamesaddon/events/InventoryListener.java
+++ src/main/java/me/harsh/privategamesaddon/events/InventoryListener.java
@@ -1,11 +1,16 @@
 package me.harsh.privategamesaddon.events;
 
+import de.marcely.bedwars.api.GameAPI;
+import de.marcely.bedwars.api.arena.Arena;
+import de.marcely.bedwars.api.arena.ArenaStatus;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.Inventory;
+import org.mineacademy.fo.menu.Menu;
 
 
 public class InventoryListener implements Listener {
@@ -14,8 +19,15 @@ public class InventoryListener implements Listener {
     @EventHandler(priority = EventPriority.HIGH)
     public void onInventoryClick(InventoryClickEvent event){
         final Inventory inventory = event.getClickedInventory();
+        final Arena arena = GameAPI.get().getArenaByPlayer((Player) event.getWhoClicked());
+        if (arena == null) return;
         if (inventory != null && inventory.getType() != InventoryType.PLAYER){
-            event.setCancelled(false);
+            if (arena.getStatus() == ArenaStatus.LOBBY){
+                final Menu menu = Menu.getMenu(((Player) event.getWhoClicked()).getPlayer());
+                if (menu != null){
+                    event.setCancelled(false);
+                }
+            }
         }
 
     }
