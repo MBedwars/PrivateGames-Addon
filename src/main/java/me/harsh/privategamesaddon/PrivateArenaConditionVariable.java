554032e67b32e18df5e0028f4030be9f347fd3ba
Marcel <8513034+MrEAlderson@users.noreply.github.com>
2024-03-12 15:53:18 +0100

    Rename condition variable to is_private

--- src/main/java/me/harsh/privategamesaddon/PrivateArenaConditionVariable.java
+++ src/main/java/me/harsh/privategamesaddon/PrivateArenaConditionVariable.java
@@ -8,7 +8,7 @@ import me.harsh.privategamesaddon.utils.Utility;
 
 public class PrivateArenaConditionVariable extends ArenaConditionVariable<ArenaConditionVariableValueNumber> {
     public PrivateArenaConditionVariable() {
-        super(PrivateGamesPlugin.getInstance(), "is_private_arena", ArenaConditionVariableValueNumber.class);
+        super(PrivateGamesPlugin.getInstance(), "is_private", ArenaConditionVariableValueNumber.class);
     }
 
     public ArenaConditionVariableValueNumber getValue(Arena arena) {
